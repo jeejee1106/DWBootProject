2c88e10cf40a81e10ea95c05feca34d32a730563
minjee <kimmj1106@naver.com>
2022-02-15 19:49:17 +0900

    handlebats Error ~ing

---
+++ src/test/java/com/jeejee/webservice/web/WebControllerTest.java
@@ -0,0 +1,25 @@
+package com.jeejee.webservice.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WebControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainPageLoading() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("Start WebService with SpringBoot");
+    }
+}
