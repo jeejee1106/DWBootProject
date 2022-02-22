a23263940db4275c53176a3945454168819de8ee
minjee <kimmj1106@naver.com>
2022-02-22 23:57:25 +0900

    @RestController, @Entity, @Bulider 복습

--- src/main/java/com/jeejee/webservice/domain/Posts.java
+++ src/main/java/com/jeejee/webservice/domain/Posts.java
@@ -7,27 +7,49 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+/**
+ * 이 클래스는 실제 DB테이블과 매칭될 클래스이며 Entity클래스라고 한다. (@Entity 어노테이션 사용하면 테이블과 자바 클래스가 매핑된다.)
+ * JPA를 사용하면 DB데이터에 작업할 경우 실제 쿼리를 날리기 보다는, 이 Entity클래스의 수정을 통해 작업한다.
+ *
+ * 즉, JPA는 자동으로 테이블을 생성하는 기능능 있다!!!
+ *
+ * JPA에서 제공하는 어노테이션
+ * @Entity : 테이블과 링크 될 클래스임을 나타낸다. 언더스코어 네이밍으로 이름을 매칭 ex) ex) TestPosts.java -> test_posts table
+ * @Id : 해당 테이블의 PK필드를 나타낸다.
+ * @GeneratedValue : PK의 생성 규칙을 나타낸다. 기본값은 AUTO. (MySql의 auto_increment와 같음)
+ * @Column : 테이블의 컬럼을 나타낸다. 굳이 추가하지 않아도 컬럼으로 지정이 되는데, 사용을 하는 경우는 기본값 외에 추강로 변경이 필요한 옵션이 있을 경우에만 사용한다.
+ */
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 자동 추가, 기본생성자의 접근 권한을 protected로 제한. Entity 클래스를 프로젝트 코드상에서 기본생성자로 생성하는 것은 막되, JPA에서 Entity 클래스를 생성하는것은 허용하기 위해 추가
 @Getter
 @Entity //테이블과 링크될 클래스임을 나타냄(언더스코어 네이밍으로 이름 매칭)
 public class Posts extends BaseTimeEntity {
 
     @Id //해당 테이블의 pk필드를 나타냄
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //pk의 생성규칙을 나타냄 - MySQL의 auto_increament와 같은 기능
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //스프링부트 2.0이상에선 옵션 추가해야함(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 500, nullable = false)
+    @Column(length = 500, nullable = false) //문자열의 경우 기본값이 VARCHAR(255)인데, 사이즈를 500으로 늘림
     private String title;
 
-    @Column(columnDefinition = "TEXT", nullable = false)
+    @Column(columnDefinition = "TEXT", nullable = false) //문자열의 경우 기본값이 VARCHAR(255)인데, 타입을 TEXT로 바꿈 (TEXT타입은 max사이즈가 무조건 65535이다.)
     private String content;
 
     private String author;
 
-    @Builder //해당 클래스의 빌더패턴 클래스를 생성, 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
+    @Builder //롬복 어노테이션 - 해당 클래스의 빌더패턴 클래스를 생성, 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
     }
 }
+
+/**
+ * 빌더 패턴의 장점
+ * 1. 필요한 데이터만 설정할 수 있음
+ * 2. 유연성을 확보할 수 있음
+ * 3. 가독성을 높일 수 있음
+ * 4. 불변성을 확보할 수 있음
+ * 참고 블로그 : https://mangkyu.tistory.com/163
+ */
--- src/main/java/com/jeejee/webservice/web/WebRestController.java
+++ src/main/java/com/jeejee/webservice/web/WebRestController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * @RestController는 @ResponseBody를 모든 메서드에 적용해준다.
+ * 즉 hello 메서드는 "HelloWorld" 라는 문자열 데이터를 JSON 형태로 반환하게 된다.
+ */
+
 @RestController
 @AllArgsConstructor //모든 필드를 인자값으로 하는 생성자를 생성
 public class WebRestController {
@@ -20,9 +25,9 @@ public class WebRestController {
     private PostsService postsService;
 
     /**
-     * 눈에 보이지는 않지만 롬복의 @AllArgsConstructor로 인해 현재 기본 생성자가 만들어진 상태다.
+     * 눈에 보이지는 않지만 롬복의 @AllArgsConstructor로 인해 현재 모든 필드 값을 파라미터로 받는 생성자가 만들어졌다.
      * 생성자가 하나일 땐 @Autowired의 생략이 가능하다.
-     * 즉, 현재 기본 생성자에 의존성 주입이 되어있는 상태이다!!
+     * 즉, 현재 생성자에 의존성 주입이 되어있는 상태이다!!
      */
 
     @GetMapping("/hello")
