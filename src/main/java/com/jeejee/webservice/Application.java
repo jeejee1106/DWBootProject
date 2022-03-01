00d94c590d2cae496717f8c92c1f73ad54699ca6
minjee <kimmj1106@naver.com>
2022-03-02 00:19:31 +0900

    JAP Auditing

--- src/main/java/com/jeejee/webservice/Application.java
+++ src/main/java/com/jeejee/webservice/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing //JPA Auditing어노테이션을 모두 활성화
+@EnableJpaAuditing //JPA Auditing어노테이션을 모두 활성화
 @SpringBootApplication //스프링부트의 자동 설정, 스프링 Bean읽기와 생성을 모두 자동으로 생성해준다.
                        //이 어노테이션이 있는 위치부터 설정을 읽기 때문에 프로젝트 최상단에 위치해야한다.
 public class Application {
---
+++ src/main/java/com/jeejee/webservice/domain/post/PostsRepository.java
@@ -0,0 +1,20 @@
+package com.jeejee.webservice.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
+
+/**
+ * 보통 ibatis/MyBatis 등에서 Dao라고 불리는 DB Layer 접근자이다.
+ * JPA에선 Repository라고 부르며 인터페이스로 생성한다.
+ * 단순히 인터페이스를 생성후, JpaRepository<Entity클래스, PK타입>를 상속하면 기본적인 CRUD 메소드가 자동생성 된다.
+ * 특별히 @Repository를 추가할 필요도 없다.
+ *
+ * 반드시 Entity 클래스와 기본 Entity Repository는 함께 위치해야 한다.
+ */
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+//    @Query("select p " + "from Posts p " + "order by p.id desc")
+//    Stream<Posts> findAllDesc();
+}
