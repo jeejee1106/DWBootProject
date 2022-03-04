d25ad3facff717a9dbdc9508ff6d636ef4357cd6
minjee <kimmj1106@naver.com>
2022-03-04 19:58:35 +0900

    구글 로그인 완성.. 감격

--- src/main/java/com/jeejee/webservice/web/MainController.java
+++ src/main/java/com/jeejee/webservice/web/MainController.java
@@ -1,6 +1,7 @@
 package com.jeejee.webservice.web;
 
 //import com.jeejee.webservice.config.auth.dto.SessionUser;
+import com.jeejee.webservice.config.auth.dto.SessionUser;
 import com.jeejee.webservice.service.PostsService;
 import com.jeejee.webservice.web.dto.PostsResponseDto;
 import lombok.AllArgsConstructor;
@@ -9,20 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @AllArgsConstructor
 public class MainController {
 
     private PostsService postsService;
-//    private HttpSession httpSession;
+    private HttpSession httpSession;
 
     @GetMapping("/")
     public String main(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
-//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-//        if (user != null) {
-//            model.addAttribute("userName", user.getName());
-//        }
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
+        //로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있다.
+
+        if (user != null) { //세션에 저장된 값이 있을 때만 model에 UserName으로 등록.
+            model.addAttribute("userName", user.getName());
+        }
         return "main"; //머스테치 스타터로 인해 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 확장자는 자동으로 지정된다.
     }
 
